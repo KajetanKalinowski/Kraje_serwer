911f87a302b3d08c9e738f3bc0495b59141b8466
KajetanKalinowski <kajtul999@gmail.com>
2023-09-21 14:43:42 +0200

    nie ok

--- index.js
+++ index.js
@@ -18,12 +18,19 @@ con.connect((err)=>{
 axios.get('https://restcountries.com/v3.1/all').then((response)=>{
     const resp = response.data
     for(var i=0;i<=resp.length-1;i++){
-    const sql = `INSERT INTO kraje(name, population, capital) VALUES ("${resp[i].name.common}", "${resp[i].population}", "${resp[i].capital}")`
+    const sql = `INSERT INTO kraje(name, population, capital, area) VALUES ("${resp[i].name.common}", "${resp[i].population}", "${resp[i].capital}","${resp[i].area}")`
+    
     con.query(sql,(err,result,fields)=>{
-        if(err) console.log("Ten element jest już w tabeli")
+        if(err)console.log(err) 
         else{console.log("Dodano pomyślnie kraj")}
     })
-    }
+}for(var i=0;i<=resp.length-1;i++){
+    const sql =` UPDATE kraje SET name="${resp[i].name.common}",population="${resp[i].population}",capital="${resp[i].capital}",area="${resp[i].area}"` 
+    con.query(sql,(err,result,fields)=>{
+        if(err)console.log(err)
+        else{console.log("Pomyślnie zaktualizowano")}
+    })
+}
     
 })
 
