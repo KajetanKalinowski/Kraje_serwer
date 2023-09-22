420041feb14856090f210b8dc251cba05463ee4d
Kajetan Kalinowski <kajetan.kalinowski@ZS1MM.LOCAL>
2023-09-22 08:54:58 +0200

    mega działa

--- app.js
+++ app.js
@@ -6,10 +6,33 @@ async function getData(){
     
     console.log(json)
     for(var i=0;i<=json.length-1;i++){
-    const body = document.getElementById("body")
+    const divradio = document.getElementById("divradio")
     const radio = document.createElement('input')
     radio.type = "radio"
+    radio.name = "radia"
+    radio.value = "radia1"
+    radio.id = json[i].kontynent
+    radio.addEventListener('change',()=>{
+        console.log(radio.id)
+        radioFetch(radio.id)
+    })
+    const label = document.createElement("label")
+    label.innerHTML = json[i].kontynent
+    divradio.appendChild(radio)
+    divradio.appendChild(label)
     }
 
 }
 getData()
+async function radioFetch(id){
+    document.getElementById("divmenu").innerHTML = ""
+    const url = await fetch(`http://localhost:3000/kontynent/${id}`)
+    const json = await url.json()
+    for(var i=0;i<=json.length-1;i++){
+        const h3 = document.createElement("h3")
+        const divmenu = document.getElementById("divmenu")
+    h3.innerHTML = json[i].name
+    divmenu.appendChild(h3)
+    }
+    
+}
